dd93e21e9995ccce8745ca2cd970910dc3b77402
Zoey Baker <zoeybaker625@gmail.com>
2023-02-09 10:46:40 -0600

    Add project files.

---
+++ WeatherAPI/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace OpenWeatherMapAPI;
+
+
+
+class program
+{
+    private static void Main(string[] args)
+    {
+        var client = new HttpClient();
+        Console.WriteLine("Please enter your API Keys");
+        var api_key = Console.ReadLine();
+
+        while (true)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Please enter the city name: ");
+            var city_name = Console.ReadLine();
+            var weatherURL = $"http://api.openweathermap.org/data/2.5/weather?q={{city_name}}&appid={{api_key}}";
+
+            var response = client.GetStringAsync(weatherURL).Result;
+            Console.WriteLine(response);
+            Console.WriteLine();
+
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
